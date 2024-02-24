ddb1d4f7e3094bd53399865a52a8117a3f0d4f53
Ahmad <scriptwithahmad@gmail.com>
2024-02-24 18:31:29 +0500

    3d Card Effect and Background with Hover bg

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "img.freepik.com",
+      "cdn.pixabay.com",
+      "images.unsplash.com",
+    ],
+  },
+};
+
+export default nextConfig;
